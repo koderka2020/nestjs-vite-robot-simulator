e8ab5b9df19b62a37e7d40b89b4255b873d7ceb2
karin illesova <karin.illesova@gmail.com>
2025-10-13 01:30:46 -0400

    Install Prisma and create Sqlite db

---
+++ prisma/migrations/20251013052817_robot_history/migration.sql
@@ -0,0 +1,9 @@
+-- CreateTable
+CREATE TABLE "RobotHistory" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "direction" TEXT NOT NULL,
+    "x" INTEGER NOT NULL,
+    "y" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
