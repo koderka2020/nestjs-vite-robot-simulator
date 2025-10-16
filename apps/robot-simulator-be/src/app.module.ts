238d0df9cb1c2133a2261f801c5d1a41b4392274
karin illesova <karin.illesova@gmail.com>
2025-10-16 01:13:19 -0400

    Configure monorepo

--- apps/robot-simulator-be/src/app.module.ts
+++ apps/robot-simulator-be/src/app.module.ts
@@ -1,9 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RobotHistoryModule } from './robot_history/robot_history.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [PrismaModule, RobotHistoryModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    RobotHistoryModule,
+  ],
   controllers: [],
   providers: [],
 })
